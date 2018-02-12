4f15061f7dca651136a46031263a474790930a13
flibustier <contact@jonathan.pl>
2018-02-12 22:11:49 +0100

    Add CORS

--- server.go
+++ server.go
@@ -6,11 +6,16 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 const (
 	port = 8008
 )
+
+func HomeHandler(rw http.ResponseWriter, r *http.Request) {
+	http.ServeFile(rw, r, "./frontend/index.html")
+}
 
 func StartServer() {
 	// Set routes
@@ -18,15 +23,25 @@ func StartServer() {
 
 	// For User Controller
 	router.HandleFunc("/users", GetUsers).Methods("GET")
-	router.Handle("/user/balance", authMiddleware(GetUserBalance)).Methods("GET")
+	router.HandleFunc("/user/balance", GetUserBalance).Methods("GET")
 	router.HandleFunc("/user/transfer", CreateUserTransaction).Methods("POST")
 	router.HandleFunc("/user/transactions", GetUserTransactions).Methods("GET")
 
 	// And for frontend
-	router.Handle("/", http.FileServer(http.Dir("./frontend/")))
-	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+
+	router.HandleFunc("/", HomeHandler)
+	router.HandleFunc("/login", HomeHandler)
+	router.HandleFunc("/callback", HomeHandler)
+	router.PathPrefix("/css/").Handler(http.StripPrefix("/css/", http.FileServer(http.Dir("./frontend/css/"))))
+	router.PathPrefix("/dist/").Handler(http.StripPrefix("/dist/", http.FileServer(http.Dir("./frontend/dist/"))))
+	router.PathPrefix("/fonts/").Handler(http.StripPrefix("/fonts/", http.FileServer(http.Dir("./frontend/fonts/"))))
+
+	// CORS
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:8080", "https://flibustier.github.io"},
+		AllowedHeaders: []string{"authorization", "id_token"},
+	})
 
 	log.Printf("[OK] Server listening on http://localhost:%d/", port)
-
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), router))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), c.Handler(authMiddleware(router))))
 }
